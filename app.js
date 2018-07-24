52532c0dd482395bcabd96456669dc15b66c9057
Michael Morgan <michael1986morgan@gmail.com>
2018-07-23 23:25:07 -0400

    created game

---
+++ app.js
@@ -0,0 +1,44 @@
+
+const selections = ['rock', 'paper', 'scissors'];
+var userChoice = document.getElementById('userChoice').value;
+let cpuChoice = '';
+let loseText = $('input', function() {
+    $(userChoice).css('color', 'red');
+});
+
+
+// get user input
+// $('button').click(function doFunction() {
+//     return userChoice
+// });
+
+// computer answer
+let num;
+function cpu(){
+    num = (Math.floor(Math.random() * 3));
+    cpuChoice = (selections[num]);
+    return cpuChoice
+}
+console.log(cpu());
+
+
+// TIE
+function tieGame(userChoice, cpuChoice) {
+    if(userChoice === cpuChoice) {
+        return 'tie'
+    }
+}
+
+// Declare winner
+function userWin(userChoice, cpuChoice) {
+    if((userChoice === 'rock' && cpuChoice === 'scissors') || (userChoice === 'paper' && cpuChoice === 'rock') || 
+    (userChoice === 'scissors' && cpuChoice === 'paper')) {
+        return 'user win';
+    } else {
+        return 'cpu win';
+    }
+};
+
+console.log(userChoice);
+
+
