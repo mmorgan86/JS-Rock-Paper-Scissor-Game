e5f64cc8424bb157e264201462b970e63c30cb63
Michael Morgan <michael1986morgan@gmail.com>
2018-07-27 16:01:23 -0400

    game created

--- app.js
+++ app.js
@@ -1,44 +1,57 @@
+let playerScore = 0
+let computerScore = 0
+let roundNumber = 1
 
-const selections = ['rock', 'paper', 'scissors'];
-let cpuChoice = '';
-let loseText = $('input', function() {
-    $(userChoice).css('color', 'red');
-});
-
-
-// get user input
-function doFunction() {
-    let userChoice = document.querySelector('input').value;
-    return (userChoice);
-};
-doFunction();
-
- 
-// computer answer
-let num;
-function cpu(){
-    num = (Math.floor(Math.random() * 3));
-    return cpuChoice = (selections[num]);
-};
-// console.log(cpu());
-
-
-// TIE
-function tieGame(userChoice, cpuChoice) {
-    if(userChoice === cpuChoice) {
-        return 'tie'
-    }
+let randomThrow = () => {
+    let randomIndex = Math.floor(Math.random() * 3)
+    return ['rock', 'paper', 'scissors'][randomIndex]
 }
 
-// Declare winner
-function userWin(userChoice, cpuChoice) {
-    if((userChoice === 'rock' && cpuChoice === 'scissors') || (userChoice === 'paper' && cpuChoice === 'rock') || 
-    (userChoice === 'scissors' && cpuChoice === 'paper')) {
-        return 'user win';
+let playRound = (playerThrow) => {
+    let result;
+    let computerThrow = randomThrow()
+
+    console.log(`Player throw: ${playerThrow} and Computer throw: ${computerThrow}`) 
+
+    if(playerThrow == computerThrow) {
+        result = "TIE"
+   } else if ((playerThrow == 'rock' && computerThrow == 'scissors') || 
+             (playerThrow == 'paper' && computerThrow == 'rock') || 
+            (playerThrow == 'scissors' && computerThrow == 'paper')) {
+                result = "PLAYER WIN!"
+                playerScore++
     } else {
-        return 'cpu win';
+        result = "COMPUTER WIN!"
+        computerScore++
     }
-};
+
+
+
+
+// UPDATE SCORE
+$('#playerScore').text(playerScore)
+$('#computerScore').text(computerScore)
+
+// UPDATE GAMELOG
+let description = `Player throw: ${playerThrow} and Computer throw: ${computerThrow}`
+
+$('tbody').prepend(`<tr><td>${roundNumber}</td><td>${description}</td><td>${result}</td><td>${playerScore} - ${computerScore}</td></tr>`)
+    roundNumber++
+}
+
+$('#rock').click (function() {
+    playRound('rock')
+})
+
+$('#paper').click (function() {
+    playRound('paper')
+})
+
+$('#scissors').click (function() {
+    playRound('scissors')
+})
+
+
 
 
 
